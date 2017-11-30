1faf27786a460055ea3d603e686084e9738e1a17
nksocha <nksocha@uwm.edu>
2017-11-29 19:24:48 -0600

    class Nov 29

--- client/src/resources/index.js
+++ client/src/resources/index.js
@@ -1,5 +1,7 @@
 export function configure(config) {
-  config.globalResources([]);
-  './value-converters/data-format';
-  './value-converters/completed';
+  config.globalResources([
+  './value-converters/date-format',
+  './value-converters/completed'
+]);
+  
 }
